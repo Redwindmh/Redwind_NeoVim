68f2887eddd5d631e3eb46b87c4500a3e7295d29
Malcolm <redwindmh@gmail.com>
2024-04-03 16:29:30 +0900

    Added noice and alphavim, which still needs to be configured...

--- lua/redwind/core/keymaps.lua
+++ lua/redwind/core/keymaps.lua
@@ -9,27 +9,26 @@ local keymap = vim.keymap
 
 --general kepmaps
 
-keymap.set("i", "jk", "<ESC>") --Exit Insert mode
+keymap.set("i", "jk", "<ESC>")                 --Exit Insert mode
 
-keymap.set("n", "<leader>nh", ":nohl<CR>") --Clear search field
+keymap.set("n", "<leader>nh", ":nohl<CR>")     --Clear search field
 
-keymap.set("n", "x", '"_x') --Delete without saving to buffer
+keymap.set("n", "x", '"_x')                    --Delete without saving to buffer
 
-keymap.set("n", "<leader>+", "<C-a>") --Increment a number up
-keymap.set("n", "<leader>-", "<C-x>") --Increment a number down
+keymap.set("n", "<leader>+", "<C-a>")          --Increment a number up
+keymap.set("n", "<leader>-", "<C-x>")          --Increment a number down
 
-keymap.set("n", "<leader>sv", "<C-w>v") --Split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") --Split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=") --Make split windows equal width
-keymap.set("n", "<leader>sx", ":close<CR>") --Close current split window
+keymap.set("n", "<leader>sv", "<C-w>v")        --Split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s")        --Split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=")        --Make split windows equal width
+keymap.set("n", "<leader>sx", ":close<CR>")    --Close current split window
 
-keymap.set("n", "<leader>to", ":tabnew<CR>") --Open new tab
+keymap.set("n", "<leader>to", ":tabnew<CR>")   --Open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") --Close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") --Go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") --Go to previous tab
+keymap.set("n", "<leader>tn", ":tabn<CR>")     --Go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>")     --Go to previous tab
 
 keymap.set('t', '<esc>', [[<C-\><C-n>]])
 
 --Plugin keymaps
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") --Maximizes current window
-
--- lua/redwind/plugins/colorscheme.lua
+++ lua/redwind/plugins/colorscheme.lua
@@ -1,8 +1,8 @@
 return {
   "bluz71/vim-nightfly-colors",
-   priority = 1000,
-   config = function()
-     vim.cmd([[colorscheme nightfly]])
-     -- vim.g.nightflyTransparent = true
-   end,
+  priority = 1000,
+  config = function()
+    vim.g.nightflyTransparent = true
+    vim.cmd([[colorscheme nightfly]])
+  end,
 }
