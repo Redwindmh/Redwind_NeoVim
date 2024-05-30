8483df30ef949e5ebac274f0524ed351eee0e2df
Malcolm <redwindmh@gmail.com>
2024-05-30 10:20:46 +0900

    Added keymap to close notifications (it's <leader>nx)

--- lua/redwind/plugins/noice.lua
+++ lua/redwind/plugins/noice.lua
@@ -7,5 +7,23 @@ return {
   dependencies = {
     "MunifTanjim/nui.nvim",
     "rcarriga/nvim-notify",
-  }
+    "hrsh7th/nvim-cmp",
+  },
+  config = function()
+    local noice = require("noice")
+    noice.setup({
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+      notify = {
+        -- Add keymap to close notifications here
+        vim.keymap.set("n", "<leader>nx", function() require("notify").dismiss() end)
+      }
+    })
+  end,
 }
